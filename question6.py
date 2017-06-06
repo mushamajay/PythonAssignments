a3dd3884e33ce386adb922aaf8e0b9294241d1d3
root <root@python.c.i-hexagon-169806.internal>
2017-06-06 09:21:53 +0000

    commit

---
+++ question6.py
@@ -0,0 +1,17 @@
+import time
+import datetime
+Name=raw_input("Enter your Name:")
+Age=int(input("Enter your Age:"))
+#print '/n'
+print 'Hi %s ,Your present Age is %d'%(Name,Age) 
+#print '/n'  
+#cd=time.strftime("%d/%m/%Y") 
+#cd-age 
+#print cd.year
+cd1=datetime.datetime.now()
+cdy=cd1.year
+by=int(cdy-Age)
+print 'Your Bday year is %d'%by
+by1=int(by+100)
+print 'Your age will be 100 years by %d'%by1
+
